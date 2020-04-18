40bb2e32292f30104a415cf0d61c0051ed11a62b
milli99 <milli.fiorino@web.de>
2020-04-18 23:47:38 +0200

    added form

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,34 @@ addButton!.onclick = () => {
   const popup = document.querySelector("#windowpop");
   popup!.classList.toggle("active");
 };
+
+//form
+const form = document.querySelector("#form");
+const productName = document.querySelector<HTMLInputElement>(".name");
+const quantity = document.querySelector<HTMLInputElement>(".quantity");
+
+form!.addEventListener("submit", function (event) {
+  event.preventDefault();
+  checkInput();
+});
+
+function checkInput() {
+  if (productName!.value === "" || quantity!.value === "") {
+    redVali(productName, "Produktname ist erforderlich");
+    redVali(quantity, "Menge ist erforderlich");
+  } else {
+    const popup = document.querySelector("#windowpop");
+    popup!.classList.remove("active");
+    const verblassen = document.querySelector("#blur");
+    verblassen!.classList.remove("active");
+
+    addElement();
+  }
+}
+
+function redVali(input: any, message: string) {
+  const formControl = input.parentElement;
+  const txt = formControl.querySelector("p");
+  formControl.className = "validation-control red";
+  txt.innerText = message;
+}
